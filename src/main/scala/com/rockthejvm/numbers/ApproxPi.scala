e0f4dbc8dcda4015b133b3f1f2f64932a9e37a08
Ethan <ethan.king@rutgers.edu>
2020-06-07 14:38:02 -0400

    updates

--- src/main/scala/com/rockthejvm/numbers/ApproxPi.scala
+++ src/main/scala/com/rockthejvm/numbers/ApproxPi.scala
@@ -2,7 +2,9 @@ package com.rockthejvm.numbers
 import scala.util.Random
 
 object ApproxPi  extends  App{
-
+  // use monte carlo method: take random point inside unit square and check to see if it lies within the circle
+  // ratio of points inside circle to all generated points is p.
+  // p*4 = pi
   def approximatePi(nPoints: Int):  Double = {
 
   }
